5b9c17e21e92c32b30c44451307d5fd42f545c20
Alfonso Rocha <alfonsoroc789@gmail.com>
2023-07-29 22:30:10 -0700

    creacion de ventana add user

--- src/index.js
+++ src/index.js
@@ -1,12 +1,57 @@
-const{app,BrowserWindow} = require('electron');
+const{app,BrowserWindow,Menu} = require('electron');
 const url = require('url');
 const path = require('path');
+const { electron } = require('process');
+
+if(process.env.NODE_ENV !=='production'){
+    require('electron-reload')(__dirname,{
+        electron: path.join(__dirname,'../node_modules','.bin','electron')
+    })
+}
 
 let mainWindow
+
+let newUserWindows
 
 
 app.on('ready', () => {
     mainWindow = new BrowserWindow({});
   
     mainWindow.loadFile(path.join(__dirname, 'views/index.html'));
+
+   const mainMenu =  Menu.buildFromTemplate(templateMneu);
+   Menu.setApplicationMenu(mainMenu)
   });
+
+function createNewUserWindows(){
+   newUserWindows = new BrowserWindow({
+        width:400,
+        height:300,
+        title:'Agregar nuevo usuario'
+    });
+    newUserWindows.setMenu(null);
+    newUserWindows.loadFile(path.join(__dirname,'views/adduser.html'));
+}
+
+
+
+
+
+
+
+
+  const templateMneu = [
+    {
+        label: 'File',
+        submenu:[
+            {
+                label:'Nuevo Usuario',
+                accelerator:'Ctrl+N',
+                click(){
+                    createNewUserWindows();
+                }
+            }
+        ]
+    }
+
+  ];
